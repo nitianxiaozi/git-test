187313c589aa0a7c359e81ea67f1b58523ecdd67
yjf <yjf@fx2022.com>
2022-04-27 18:03:40 +0800

    second commit

--- git-test-example/src/main/java/com/fx/GitTest.java
+++ git-test-example/src/main/java/com/fx/GitTest.java
@@ -3,5 +3,6 @@ package com.fx;
 public class GitTest {
     public static void main(String[] args) {
         System.out.printf("", "hello git in the idea");
+        System.out.printf("", "hello git in the idea2");
     }
 }
