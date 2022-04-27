b83286c02b3df8562518cf2bd79a0207b6a34b7d
yjf <yjf@fx2022.com>
2022-04-27 18:15:58 +0800

    third commit by hot-fix

--- git-test-example/src/main/java/com/fx/GitTest.java
+++ git-test-example/src/main/java/com/fx/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.printf("", "hello git in the idea");
         System.out.printf("", "hello git in the idea2");
+        System.out.printf("hot-fix modify");
     }
 }
